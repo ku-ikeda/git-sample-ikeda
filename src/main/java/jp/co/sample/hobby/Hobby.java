683f5ce15c061a3479e90869120598b850f251ca
yuta.ikeda <yuta.ikeda@rakus.co.jp>
2018-10-30 12:25:38 +0900

    新たにhobbyクラスを作成

---
+++ src/main/java/jp/co/sample/hobby/Hobby.java
@@ -0,0 +1,42 @@
+package jp.co.sample.hobby;
+
+public class Hobby {
+
+	private Integer id;
+	private String name;
+	private String gnder;
+	private Integer age;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGnder() {
+		return gnder;
+	}
+
+	public void setGnder(String gnder) {
+		this.gnder = gnder;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+}
