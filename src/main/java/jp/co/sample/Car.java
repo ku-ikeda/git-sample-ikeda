f16a01b44f0f5c8603da005735e0324be159500c
yuta.ikeda <yuta.ikeda@rakus.co.jp>
2018-10-30 11:25:48 +0900

    新規にCarクラスを作成　及び　スタートプログラムにコメントを出力するように修正

---
+++ src/main/java/jp/co/sample/Car.java
@@ -0,0 +1,17 @@
+package jp.co.sample;
+
+public class Car {
+
+	//スピード
+	private Integer speed;
+
+	public Integer getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+	
+	
+}
--- src/main/java/jp/co/sample/GitSampleIkedaApplication.java
+++ src/main/java/jp/co/sample/GitSampleIkedaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitSampleIkedaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("デプロイとサーバー起動を行います");
 		SpringApplication.run(GitSampleIkedaApplication.class, args);
 	}
 }
